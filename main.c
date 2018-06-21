650378b2bb2459fbc101bf60feafbc2d6006e88d
20171105123 <s20171105123@admindeiMac-34.local>
2018-06-21 10:05:33 +0800

    TFTFFF

---
+++ main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  test6.21
+//
+//  Created by 鲁祥 on 2018/6/21.
+//  Copyright © 2018年 鲁祥. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("hello, skaldfhh\n");
+    return 0;
+}
